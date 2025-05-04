d0440198ae5e79b1e9c7acf88b9fbfca3c0c862a
Philipp Tanlak <philipp.tanlak@gmail.com>
2025-05-04 22:56:32 +0200

    better term

---
+++ README.md
@@ -0,0 +1,45 @@
+## God, as in (Go) (D)ebugger
+
+... maybe I'll think of a better name in the future.
+
+### Why
+
+- Delve is a bit clunky to use and I don't like vscode or golan*d*.
+- gdb-dashboard looks cool and gets the job done.
+
+## Screenshot
+
+![Screenshot](/.github/screenshot.png)
+
+## Commands
+
+| Command             | Alias(es)         | Description                                                                              |
+| :------------------ | :---------------- | :--------------------------------------------------------------------------------------- |
+| `continue`          | `c`               | Continue program execution until the next breakpoint or program termination.             |
+| `next`              | `n`               | Step to the next source line in the current function, stepping *over* function calls.      |
+| `step`              | `s`               | Step to the next source line, stepping *into* function calls.                            |
+| `stepout`           | `so`              | Continue execution until the current function returns.                                   |
+| `nexti`             | `ni`              | Step to the next CPU instruction, stepping *over* function calls.                        |
+| `stepi`             | `si`              | Step to the next CPU instruction, stepping *into* function calls.                        |
+| `quit`              | `q`               | Exit the debugger session.                                                               |
+| `pane source`       | `pane src`        | Toggle the visibility of the source code pane.                                           |
+| `pane assembly`     | `pane asm`        | Toggle the visibility of the assembly code pane.                                         |
+| `pane variables`    | `pane vars`       | Toggle the visibility of the variables pane.                                             |
+| `pane breakpoints`  | `pane bp`         | Toggle the visibility of the breakpoints pane.                                           |
+| `grow source`       | `grow src`        | Increase the height of the source code pane by 2 lines.                                  |
+| `grow assembly`     | `grow asm`        | Increase the height of the assembly code pane by 2 lines.                                |
+| `shrink source`     | `shrink src`      | Decrease the height of the source code pane by 2 lines (minimum height is 1).            |
+| `shrink assembly`   | `shrink asm`      | Decrease the height of the assembly code pane by 2 lines (minimum height is 1).          |
+| `toggle <id>`       | `t <id>`          | Toggle the enabled/disabled state of the breakpoint with the specified numeric ID.         |
+| `break <location>`  | `b <location>`    | Create a breakpoint at the specified location. `<location>` can be `file:line`, `line` (in the current file), or a `functionName`. |
+| `clear <id>`        | `c <id>`          | Remove (clear) the breakpoint with the specified numeric ID.                             |
+| `(empty command)`   |                   | Repeat the last command that was entered.                                                |
+
+**Notes:**
+
+* `<id>` refers to the numeric identifier of a breakpoint.
+* `<location>` specifies where to set a breakpoint.
+
+## Disclaimer
+
+This is early stage, just a couple hours in. Pls don't read the code. Use at your own risk.
---
+++ main.go
@@ -0,0 +1,486 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/go-delve/delve/service/api"
+	"github.com/go-delve/delve/service/rpc2"
+	"golang.org/x/term"
+)
+
+type DebugState struct {
+	File        string
+	Line        int
+	GoroutineID int64
+	VarsLocal   []api.Variable
+	VarsFunc    []api.Variable
+	Breakpoints []*api.Breakpoint
+	Assembly    []api.AsmInstruction
+}
+
+type TermState struct {
+	LastCommand     string
+	PaneSource      bool
+	PaneAssembly    bool
+	PaneVars        bool
+	PaneBreakpoints bool
+	HeightSource    int
+	HeightAssembly  int
+}
+
+var normalLoadConfig = api.LoadConfig{
+	FollowPointers:     true,
+	MaxVariableRecurse: 1,
+	MaxStringLen:       64,
+	MaxArrayValues:     64,
+	MaxStructFields:    -1,
+}
+
+func main() {
+	width, _, _ := term.GetSize(1)
+	stdin := ListenStdin()
+
+	dlv := rpc2.NewClient("127.0.0.1:6060")
+	dlv.CreateBreakpoint(&api.Breakpoint{FunctionName: "main.main"})
+
+	term := NewTermState()
+
+	for {
+		state, err := GetState(dlv)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		w := &strings.Builder{}
+		w.Write(ClearScreen)
+		w.Write(ClearScreenFull)
+
+		if term.PaneSource {
+			printSource(w, state.File, state.Line, state.Breakpoints, width, term.HeightSource)
+		}
+		if term.PaneAssembly {
+			printAssembly(w, state.Assembly, width, term.HeightAssembly)
+		}
+		if term.PaneVars {
+			printVars(w, state.VarsLocal, state.VarsFunc, width)
+		}
+		if term.PaneBreakpoints {
+			printBreakpoints(w, state.Breakpoints, width)
+		}
+
+		printLine(w, width)
+		printReadLine(w)
+
+		os.Stdout.WriteString(w.String())
+
+		cmd, ok := <-stdin
+		if !ok {
+			break
+		}
+
+		term, ok = Update(term, state, dlv, cmd)
+		if !ok {
+			return
+		}
+	}
+
+}
+
+func NewTermState() TermState {
+	return TermState{
+		LastCommand:     "?",
+		PaneSource:      true,
+		PaneAssembly:    true,
+		PaneVars:        true,
+		PaneBreakpoints: true,
+		HeightSource:    9,
+		HeightAssembly:  9,
+	}
+}
+
+func Update(term TermState, debug DebugState, dlv *rpc2.RPCClient, cmd string) (TermState, bool) {
+	switch cmd {
+	case "c", "continue":
+		s := <-dlv.Continue()
+		if s.Exited {
+			return term, false
+		}
+	case "n", "next":
+		dlv.Next()
+	case "s", "step":
+		dlv.Step()
+	case "so", "stepout":
+		dlv.StepOut()
+	case "ni", "nexti":
+		dlv.StepInstruction(true)
+	case "si", "stepi":
+		dlv.StepInstruction(false)
+	case "q", "quit":
+		return term, false
+	case "pane src", "pane source":
+		term.PaneSource = !term.PaneSource
+	case "pane asm", "pane assembly":
+		term.PaneAssembly = !term.PaneAssembly
+	case "pane vars", "pane variables":
+		term.PaneVars = !term.PaneVars
+	case "pane bp", "pane breakpoints":
+		term.PaneBreakpoints = !term.PaneBreakpoints
+	case "grow src", "grow source":
+		term.HeightSource += 2
+	case "grow asm", "grow assembly":
+		term.HeightAssembly += 2
+	case "shrink src", "shrink source":
+		term.HeightSource = max(1, term.HeightSource-2)
+	case "shrink asm", "shrink assembly":
+		term.HeightAssembly = max(1, term.HeightAssembly-2)
+	case "":
+		return Update(term, debug, dlv, term.LastCommand)
+	default:
+		if strings.HasPrefix(cmd, "t ") {
+			cmd = strings.Replace(cmd, "t ", "toggle ", 1)
+		} else if strings.HasPrefix(cmd, "c ") {
+			cmd = strings.Replace(cmd, "c ", "clear ", 1)
+		} else if strings.HasPrefix(cmd, "b ") {
+			cmd = strings.Replace(cmd, "b ", "break ", 1)
+		}
+
+		if after, ok := strings.CutPrefix(cmd, "toggle "); ok {
+			id, _ := strconv.Atoi(after)
+			dlv.ToggleBreakpoint(id)
+		} else if after, ok := strings.CutPrefix(cmd, "break "); ok {
+			if file, linestr, ok := strings.Cut(after, ":"); ok {
+				line, _ := strconv.Atoi(linestr)
+				if !strings.Contains(file, "/") {
+					file = filepath.Join(filepath.Dir(debug.File), file)
+				}
+				dlv.CreateBreakpoint(&api.Breakpoint{
+					File: file,
+					Line: line,
+				})
+			} else {
+				line, err := strconv.Atoi(after)
+				if err != nil {
+					dlv.CreateBreakpoint(&api.Breakpoint{FunctionName: after})
+				} else {
+					dlv.CreateBreakpoint(&api.Breakpoint{
+						File: debug.File,
+						Line: line,
+					})
+				}
+			}
+		} else if after, ok := strings.CutPrefix(cmd, "clear "); ok {
+			id, _ := strconv.Atoi(after)
+			dlv.ClearBreakpoint(id)
+		}
+	}
+
+	term.LastCommand = cmd
+	return term, true
+}
+
+func GetState(dlv *rpc2.RPCClient) (DebugState, error) {
+	ds, err := dlv.GetState()
+	if err != nil {
+		return DebugState{}, err
+	}
+
+	var state DebugState
+	state.GoroutineID = ds.CurrentThread.GoroutineID
+	state.File = ds.CurrentThread.File
+	state.Line = ds.CurrentThread.Line
+
+	state.Breakpoints, err = dlv.ListBreakpoints(true)
+	if err != nil {
+		return DebugState{}, err
+	}
+	sort.Slice(state.Breakpoints, func(i, j int) bool {
+		return state.Breakpoints[i].ID < state.Breakpoints[j].ID
+	})
+
+	evalScope := api.EvalScope{GoroutineID: state.GoroutineID}
+
+	if state.GoroutineID != 0 {
+		state.VarsLocal, err = dlv.ListLocalVariables(evalScope, normalLoadConfig)
+		if err != nil {
+			return DebugState{}, err
+		}
+		state.VarsFunc, err = dlv.ListFunctionArgs(evalScope, normalLoadConfig)
+		if err != nil {
+			return DebugState{}, err
+		}
+		state.Assembly, err = dlv.DisassemblePC(evalScope, ds.CurrentThread.PC, api.GoFlavour)
+		if err != nil {
+			return DebugState{}, err
+		}
+	}
+
+	return state, nil
+}
+
+func printBreakpoints(w *strings.Builder, breakpoints []*api.Breakpoint, width int) {
+	printHeader(w, "Breakpoints", width)
+	tabw := tabwriter.NewWriter(w, 0, 1, 1, ' ', 0)
+
+	for _, bp := range breakpoints {
+		if bp.ID > 0 {
+			fmt.Fprintf(tabw, "%s%d\t%s%s:%d (%s)\n", ColorFGGray, bp.ID, ColorFGWhite, filepath.Base(bp.File), bp.Line, bp.FunctionName)
+
+		}
+	}
+
+	tabw.Flush()
+}
+
+func printAssembly(w *strings.Builder, instructions api.AsmInstructions, width, height int) {
+	printHeader(w, "Assembly", width)
+	w.Write(ColorFGGray)
+
+	pc := 0
+	for i, inst := range instructions {
+		if inst.AtPC {
+			pc = i
+			break
+		}
+	}
+
+	start := max(0, pc-height/2)
+	end := min(start+height, len(instructions))
+
+	tabw := tabwriter.NewWriter(w, 24, 1, 4, ' ', 0)
+
+	for i := start; i < end; i++ {
+		inst := instructions[i]
+
+		// PC
+		if inst.AtPC {
+			tabw.Write(ColorFGCyan)
+		} else if inst.Breakpoint {
+			tabw.Write(ColorFGMagenta)
+		} else {
+			tabw.Write(ColorFGGray)
+		}
+		fmt.Fprintf(tabw, "0x%x    ", inst.Loc.PC)
+
+		// Bytes
+		tabw.Write(ColorFGGray)
+		for i, b := range inst.Bytes {
+			if i == len(inst.Bytes)-1 {
+				fmt.Fprintf(tabw, "%02x", b)
+			} else {
+				fmt.Fprintf(tabw, "%02x ", b)
+			}
+		}
+		tabw.Write([]byte("\t"))
+
+		// Text
+		if inst.AtPC {
+			tabw.Write(ColorFGCyan)
+		} else {
+			tabw.Write(ColorFGWhite)
+		}
+		segs := strings.SplitN(inst.Text, " ", 2)
+		tabw.Write([]byte(segs[0]))
+		tabw.Write([]byte("\t"))
+
+		if len(segs) == 2 {
+			tabw.Write([]byte(segs[1]))
+			tabw.Write([]byte("\t"))
+		}
+
+		// Source
+		fmt.Fprintf(tabw, "%s:%d\n", filepath.Base(inst.Loc.File), inst.Loc.Line)
+	}
+	tabw.Flush()
+}
+
+func printVars(w *strings.Builder, locals, funcs []api.Variable, width int) {
+	printHeader(w, "Variables", width)
+
+	vars := [][]api.Variable{funcs, locals}
+	name := []string{"fun ", "loc "}
+
+	for i := range vars {
+		for _, v := range vars[i] {
+			w.Write(ColorFGGray)
+			w.WriteString(name[i])
+			w.Write(ColorFGWhite)
+			w.WriteString(v.Name)
+			w.Write(ColorFGGray)
+			w.WriteString(" = ")
+			w.Write(ColorFGWhite)
+
+			s := v.SinglelineStringWithShortTypes()
+			if len(s) > width-len(v.Name)-7 {
+				s = s[:width-len(v.Name)-7]
+			}
+
+			w.WriteString(s)
+			w.WriteString("\n")
+		}
+	}
+}
+
+func printSource(w *strings.Builder, file string, pc int, breakpoints []*api.Breakpoint, width, height int) {
+	printHeader(w, "Source", width)
+
+	bpmap := make(map[int]*api.Breakpoint, len(breakpoints))
+	for _, bp := range breakpoints {
+		if bp.File == file {
+			bpmap[bp.Line-1] = bp
+		}
+	}
+
+	lines := readFileLines(file)
+	currLine := pc - 1
+
+	start := max(0, currLine-height/2)
+	end := min(start+height, len(lines))
+	digits := max(3, numDigits(end))
+	iotaBuf := make([]byte, digits)
+	for i := range digits {
+		iotaBuf[i] = ' '
+	}
+
+	for i := start; i < end; i++ {
+		iota(iotaBuf, i+1)
+		if i == currLine {
+			w.Write(ColorFGCyan)
+		} else if _, ok := bpmap[i]; ok {
+			w.Write(ColorFGMagenta)
+		} else {
+			w.Write(ColorFGGray)
+		}
+
+		w.Write(iotaBuf)
+		w.WriteString(" ")
+		if i == currLine {
+			w.Write(ColorFGCyan)
+		} else {
+			w.Write(ColorFGWhite)
+		}
+
+		if len(lines[i]) > width-3 {
+			w.WriteString(lines[i][:width-3-digits])
+		} else {
+			w.WriteString(lines[i])
+		}
+		w.WriteString("\n")
+	}
+}
+
+func printHeader(w *strings.Builder, title string, width int) {
+	prefix := 3
+
+	w.Write(ColorFGCyan)
+	w.WriteString(strings.Repeat("━", prefix))
+	w.Write(ColorFGMagenta)
+	w.WriteString(" ")
+	w.WriteString(title)
+	w.WriteString(" ")
+	w.Write(ColorFGCyan)
+	w.WriteString(strings.Repeat("━", width-len(title)-2-prefix))
+}
+
+func printLine(w *strings.Builder, width int) {
+	w.Write(ColorFGCyan)
+	w.WriteString(strings.Repeat("━", width))
+}
+
+func printReadLine(w *strings.Builder) {
+	w.Write(ColorFGMagenta)
+	w.WriteString(">>> ")
+	w.Write(ColorFGWhite)
+}
+
+func numDigits(i int) int {
+	if i == 0 {
+		return 1
+	}
+	count := 0
+	for i != 0 {
+		i /= 10
+		count++
+	}
+	return count
+}
+
+func iota(buf []byte, n int) {
+	for i := len(buf) - 1; i >= 0; i-- {
+		if n > 0 {
+			buf[i] = byte(n%10) + '0'
+			n = n / 10
+		} else {
+			buf[i] = ' '
+		}
+	}
+
+}
+
+var filecache = map[string][]string{}
+
+func ListenStdin() <-chan string {
+	stdin := bufio.NewReader(os.Stdin)
+	cmd := make(chan string)
+
+	go func() {
+		for {
+			str, err := stdin.ReadString('\n')
+			if err != nil {
+				close(cmd)
+				return
+			}
+			select {
+			case cmd <- strings.TrimSpace(str):
+			default:
+			}
+		}
+	}()
+
+	return cmd
+}
+
+func readFileLines(file string) []string {
+	if lines, ok := filecache[file]; ok {
+		return lines
+	}
+
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return []string{}
+	}
+	src := string(data)
+	src = strings.ReplaceAll(src, "\t", "    ")
+	lines := strings.Split(src, "\n")
+	filecache[file] = lines
+	return lines
+}
+
+var (
+	AltScreen       = []byte("\033[?1049h")
+	ExitAltScreen   = []byte("\033[?1049l")
+	ClearScreen     = []byte("\033[2J")
+	ClearScreenFull = []byte("\033[H")
+	ClearLine       = []byte("\033[2K")
+	ResetCursor     = []byte("\033[1;1H")
+	ShowCursor      = []byte("\033[?25h")
+	HideCursor      = []byte("\033[?25l")
+
+	ColorReset     = []byte("\033[m")
+	ColorFGGray    = []byte("\033[38;37m")
+	ColorFGBlack   = []byte("\033[38;90m")
+	ColorFGRed     = []byte("\033[38;91m")
+	ColorFGGreen   = []byte("\033[38;92m")
+	ColorFGYellow  = []byte("\033[38;93m")
+	ColorFGBlue    = []byte("\033[38;94m")
+	ColorFGMagenta = []byte("\033[38;95m")
+	ColorFGCyan    = []byte("\033[38;96m")
+	ColorFGWhite   = []byte("\033[38;97m")
+)
